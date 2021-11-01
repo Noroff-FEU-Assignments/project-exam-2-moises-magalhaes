da60348ee0d5e37ea887fec8a82a13fc62fbca06
Moisés Arão Rodrigues Magalhães <moises.arao@hotmail.com>
2021-11-01 15:11:43 +0100

    navigation menu

---
+++ vida-em-movimento/components/layout/NavigationMenu.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import Link from "next/link";
+
+export const NavigationMenu = () => {
+	return (
+		<>
+			<Navbar bg="dark" variant="dark">
+				<Container>
+					<Navbar.Brand href="/">
+						<Navbar.Brand href="/">Vida em Movimento</Navbar.Brand>
+						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+						<Navbar.Collapse id="basic-navbar-nav">
+							<Nav className="me-auto">
+								<Link href="/" passHref>
+									<Nav.Link>Home</Nav.Link>
+								</Link>
+								<Link href="/about" passHref>
+									<Nav.Link>About</Nav.Link>
+								</Link>
+
+								<Link href="/services and products" passHref>
+									<Nav.Link>Products and services</Nav.Link>
+								</Link>
+
+								<Link href="/events" passHref>
+									<Nav.Link>Events</Nav.Link>
+								</Link>
+
+								<Link href="/contact" passHref>
+									<Nav.Link>Contacts</Nav.Link>
+								</Link>
+								<Link href="/cart" passHref>
+									<Nav.Link>Cart</Nav.Link>
+								</Link>
+								<Link href="/login" passHref>
+									<Nav.Link>Login</Nav.Link>
+								</Link>
+							</Nav>
+						</Navbar.Collapse>
+					</Navbar.Brand>
+				</Container>
+			</Navbar>
+		</>
+	);
+};
