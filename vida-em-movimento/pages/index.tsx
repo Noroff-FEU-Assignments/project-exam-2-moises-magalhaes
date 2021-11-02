5139807abda18af64b1ba4c8a1f9a8eb99da7e0d
Moisés Arão Rodrigues Magalhães <moises.arao@hotmail.com>
2021-11-02 10:15:09 +0100

    adding Layout

--- vida-em-movimento/pages/index.tsx
+++ vida-em-movimento/pages/index.tsx
@@ -2,15 +2,13 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import HeroBanner from "../components/layout/HeroBanner";
-import { NavigationMenu } from "../components/layout/NavigationMenu";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
 	return (
-		<div>
-			<NavigationMenu />
+		<>
 			<HeroBanner />
-		</div>
+		</>
 	);
 };
 
