80700c9398374c9bd04149834ac06e10be067701
Moisés Arão Rodrigues Magalhães <moises.arao@hotmail.com>
2021-11-02 09:40:02 +0100

    navigation menu

--- vida-em-movimento/components/layout/NavigationMenu.tsx
+++ vida-em-movimento/components/layout/NavigationMenu.tsx
@@ -5,40 +5,38 @@ import Link from "next/link";
 export const NavigationMenu = () => {
 	return (
 		<>
-			<Navbar bg="dark" variant="dark">
+			<Navbar bg="dark" expand="lg" variant="dark">
 				<Container>
-					<Navbar.Brand href="/">
-						<Navbar.Brand href="/">Vida em Movimento</Navbar.Brand>
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
-						<Navbar.Collapse id="basic-navbar-nav">
-							<Nav className="me-auto">
-								<Link href="/" passHref>
-									<Nav.Link>Home</Nav.Link>
-								</Link>
-								<Link href="/about" passHref>
-									<Nav.Link>About</Nav.Link>
-								</Link>
+					<Navbar.Brand href="/">Vida em Movimento</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Link href="/" passHref>
+								<Nav.Link>Home</Nav.Link>
+							</Link>
+							<Link href="/about" passHref>
+								<Nav.Link>About</Nav.Link>
+							</Link>
 
-								<Link href="/services and products" passHref>
-									<Nav.Link>Products and services</Nav.Link>
-								</Link>
+							<Link href="/services and products" passHref>
+								<Nav.Link>Products and services</Nav.Link>
+							</Link>
 
-								<Link href="/events" passHref>
-									<Nav.Link>Events</Nav.Link>
-								</Link>
+							<Link href="/events" passHref>
+								<Nav.Link>Events</Nav.Link>
+							</Link>
 
-								<Link href="/contact" passHref>
-									<Nav.Link>Contacts</Nav.Link>
-								</Link>
-								<Link href="/cart" passHref>
-									<Nav.Link>Cart</Nav.Link>
-								</Link>
-								<Link href="/login" passHref>
-									<Nav.Link>Login</Nav.Link>
-								</Link>
-							</Nav>
-						</Navbar.Collapse>
-					</Navbar.Brand>
+							<Link href="/contact" passHref>
+								<Nav.Link>Contacts</Nav.Link>
+							</Link>
+							<Link href="/cart" passHref>
+								<Nav.Link>Cart</Nav.Link>
+							</Link>
+							<Link href="/login" passHref>
+								<Nav.Link>Login</Nav.Link>
+							</Link>
+						</Nav>
+					</Navbar.Collapse>
 				</Container>
 			</Navbar>
 		</>
--- vida-em-movimento/pages/index.tsx
+++ vida-em-movimento/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import HeroBanner from "../components/layout/HeroBanner";
 import { NavigationMenu } from "../components/layout/NavigationMenu";
 import styles from "../styles/Home.module.css";
 
@@ -8,6 +9,7 @@ const Home: NextPage = () => {
 	return (
 		<div>
 			<NavigationMenu />
+			<HeroBanner />
 		</div>
 	);
 };
